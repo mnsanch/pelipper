31bb4a28702f4b9a65711b1bc6ef880fcba22219
Marc <marc@prueba.com>
2024-02-27 19:19:19 +0100

    Base de datos 1er intento

---
+++ database/migrations/2024_02_27_161940_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pppcomments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('ID_User');
+            $table->foreign('ID_User')->references('id')->on('pppussers')->onDelete('cascade');
+            $table->unsignedBigInteger('ID_Post');
+            $table->string('Comment');
+            $table->integer('Upvote');
+            $table->integer('Downvote');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pppcomments');
+    }
+};
