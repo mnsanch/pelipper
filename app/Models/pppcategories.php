2e88f3af7430740d5f0edc3fb06f2008edc622c2
Marc <marc@prueba.com>
2024-03-15 17:12:23 +0100

    para el aitor

--- app/Models/pppcategories.php
+++ app/Models/pppcategories.php
@@ -8,5 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class pppcategories extends Model
 {
     use HasFactory;
+    protected $fillable = ['Category_Name'];
+
+    public function posts()
+    {
+        return $this->belongsToMany(pppposts::class, 'ppppost_categories');
+    }
 
 }
