9181947ba8b2b0bebf14873d75429b0dbc835b3d
Marc <marc@prueba.com>
2024-04-03 16:49:21 +0200

    w

--- resources/js/views/home/index.vue
+++ resources/js/views/home/index.vue
@@ -1,7 +1,4 @@
 <template>
-    <div class="layout-sidebar">
-            <app-sidebar></app-sidebar>
-        </div>
     <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-center pt-8 sm:text-gray-600 dark:text-gray-400 text-sm">
