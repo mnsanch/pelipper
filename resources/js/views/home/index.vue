de0af57c8a6aa46daec9b3b1d3f745842e95f13c
Marc <marc@prueba.com>
2024-04-15 19:02:33 +0200

    poder editar los posts desde el home

--- resources/js/views/home/index.vue
+++ resources/js/views/home/index.vue
@@ -197,13 +197,13 @@
                         <!-- Botones edit + delete -->
                         <div class="d-flex d-flex flex-row align-items-end">
                             <!--Boton edit-->
-                            <div class="box-40" v-if="post.ID_User==user.id" @click="editPost(post.id)">
+                            <router-link v-if="post.ID_User==user.id" :to="{ name: 'prueba.edit', params: { id: post.id } }" class="box-40">
                                 <button onclick="" class="post-edit-button p-0 d-flex justify-content-center align-items-center">
                                     <svg rpl="" fill="currentColor" height="20" icon-name="edit-outline" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg">
                                         <path d="m18.236 3.158-1.4-1.4a2.615 2.615 0 0 0-3.667-.021L1.336 13.318a1.129 1.129 0 0 0-.336.8v3.757A1.122 1.122 0 0 0 2.121 19h3.757a1.131 1.131 0 0 0 .8-.337L18.256 6.826a2.616 2.616 0 0 0-.02-3.668ZM5.824 17.747H2.25v-3.574l9.644-9.435L15.259 8.1l-9.435 9.647ZM17.363 5.952l-1.23 1.257-3.345-3.345 1.257-1.23a1.362 1.362 0 0 1 1.91.01l1.4 1.4a1.364 1.364 0 0 1 .008 1.908Z"></path>
                                     </svg>
                                 </button>
-                            </div>
+                            </router-link>
                             <!--Boton delete-->
                             <div class="box-40 mx-0" v-if="post.ID_User==user.id" @click="deletePosthome(post.id)">
                                 <button onclick="" class="post-delete-button p-0 d-flex justify-content-center align-items-center">
